5d59f8c1d3421ab1baa129610180862a08e4e1de
vlakshmi2003 <vijayalakshmib112@gmail.com>
2023-07-24 18:00:21 +0530

    second commit

--- src/saranya.jsx
+++ src/saranya.jsx
@@ -2,9 +2,13 @@ import React from "react"
 const Saranya =()=>{
     return(
         <div>
+            online sales app
+            <br/>
             Amazon
             Flipkart
             Meesho
+            nyka
+            purple
         </div>
     )
 }
